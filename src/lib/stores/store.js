06f277c42c2cd7958d2f2c0ddf891fb6ea57bae3
Thiemann96 <e_thie10@uni-muenster.de>
2024-03-01 14:47:07 +0100

    edited terminology

--- src/lib/stores/store.js
+++ src/lib/stores/store.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 
 export const useBoardStore = create ((set)=>({
-    board: 'senseBox MCU',
+    board: 'senseBoxMCU',
     setBoard: (board) => set({board}),
     
 }))
