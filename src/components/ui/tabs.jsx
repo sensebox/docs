f9efbed70563b28823f6a31a9a1d144b5a2f28ac
Thiemann96 <erictg96@gmail.com>
2024-03-05 15:42:35 +0100

    updated tabs to arduino install

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -9,7 +9,7 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "grid grid-flow-col h-10  justify-stretch gap-2 rounded-md p-1 text-muted-foreground",
+      "grid grid-flow-col h-10  justify-stretch gap-2 rounded-md p-1  text-muted-foreground",
       className
     )}
     {...props}
@@ -21,7 +21,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex justify-center text-center whitespace-nowrap rounded-lg px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-blue data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex justify-center text-center whitespace-nowrap rounded-lg px-3 py-1.5 text-sm font-medium bg-slate-100 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-blue data-[state=active]:text-foreground data-[state=active]:shadow-sm",
       className
     )}
     {...props}
