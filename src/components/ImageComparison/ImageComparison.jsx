0460c3e3f8916c13fc35d562df26d61a3b3aec0e
Thiemann96 <e.thieme@reedu.de>
2025-12-17 12:38:48 +0100

    styles: for small displays

--- src/components/ImageComparison/ImageComparison.jsx
+++ src/components/ImageComparison/ImageComparison.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
+import React from 'react'
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from 'react-compare-slider'
 import { cx } from 'class-variance-authority'
 
-export default function ImageComparison({ leftImage, rightImage, leftLabel, rightLabel, size }) {
+export default function ImageComparison({
+  leftImage,
+  rightImage,
+  leftLabel,
+  rightLabel,
+  size,
+}) {
   return (
     <div className="flex flex-col items-center">
       <ReactCompareSlider
         itemOne={
-            <ReactCompareSliderImage 
-                src={leftImage} 
-                alt={leftLabel || "Bild Links"} 
-            />
+          <ReactCompareSliderImage
+            src={leftImage}
+            alt={leftLabel || 'Bild Links'}
+          />
         }
         itemTwo={
-            <ReactCompareSliderImage 
-                src={rightImage} 
-                alt={rightLabel || "Bild Rechts"} 
-            />
+          <ReactCompareSliderImage
+            src={rightImage}
+            alt={rightLabel || 'Bild Rechts'}
+          />
         }
-        className={cx(size === 'xl' ? 'w-[45rem] ' : 'w-[25rem]')}
+        className={cx(size === 'xl' ? 'w-[45rem] ' : 'w-[20rem]')}
       />
     </div>
-  );
+  )
 }
