dd2f6038b95601d57e0bf99054427fed42dceb0d
lav-sen <lavinia.sen0@gmail.com>
2025-12-22 13:33:02 +0100

    prettier buttons finish

--- src/components/ButtonWithText/buttonWithText.jsx
+++
@@ -1,7 +0,0 @@
-const ButtonWithText = ({ title }) => (
-<button type="button" 
-class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-    { title } </button>
-);
-
-export default ButtonWithText;
