fce73b633daa48d3c75e50945fcd17f661d1c8cd
Lav-Sen <lavinia.sen0@gmail.com>
2026-02-02 10:29:59 +0100

    bug fix sidebar

--- sidebars/homeSidebar.js
+++ sidebars/homeSidebar.js
@@ -5,7 +5,125 @@ const homeSidebar = {
   link: {
     type: 'doc',
     id: '/products/home/home-overview.mdx',
-  }
+  },
+  items: [
+    {
+      type: 'category',
+      label: 'Erste Schritte',
+	  key: 'edu-gettingStarted',
+      items: [
+        'boards/mcu/mcu-revisions',
+        'boards/mcu/mcu-in-betrieb-nehmen',
+        'boards/mcu/mcu-erster-sketch',
+        'boards/mcu/mcu-kompilieren',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Hardware',
+	  key: 'edu-hardware',
+      link: {
+        type: 'generated-index',
+        title: 'Hardware',
+      },
+      items: [
+        {
+          type: 'category',
+          label: 'in der :edu enthaltene Hardware',
+		  key: 'edu-included-hardware',
+          link: {
+            type: 'generated-index',
+            title: 'in der :edu enthaltene Hardware',
+          },
+          items: [
+            'hardware/bee/wifi-bee',
+            'hardware/bee/sd-bee',
+            'hardware/sensors/temperatur-luftfeuchte',
+            'hardware/sensors/helligkeit-uv',
+            'hardware/sensors/distanz',
+            'hardware/sensors/mikro',
+            'hardware/sensors/luftdruck-temperatur',
+            'hardware/accessoires/display',
+            'hardware/sensors/bmx055',
+			'hardware/accessoires/cable_overview',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'zusätzlich erwerbbare Hardware',
+		  key: 'edu-additional-hardware',
+          link: {
+            type: 'generated-index',
+            title: 'zusätzlich erwerbbare Hardware',
+          },
+          items: [
+            'hardware/sensors/feinstaub-sds011',
+            'hardware/sensors/feinstaub-sps30',
+            'hardware/bee/lora-bee',
+            'hardware/sensors/co2',
+            'hardware/sensors/truebner',
+            'hardware/sensors/umweltsensor',
+			'hardware/sensors/niederschlag-rg15',
+            'hardware/bee/ethernet-bee',
+            'hardware/accessoires/gps',
+            'hardware/sensors/wassertemperatur',
+			'hardware/sensors/niederschlag-rg15',
+            'hardware/accessoires/multiplexer',
+			'hardware/accessoires/solar_set'
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Blockly',
+	  key: 'edu-blockly',
+      link: {
+        type: 'generated-index',
+        title: 'Blockly',
+      },
+      items: [
+        'editors/blockly/blocks/logik',
+        'editors/blockly/blocks/mathematik',
+        'editors/blockly/blocks/schleifen',
+        'editors/blockly/blocks/sensoren',
+        'editors/blockly/blocks/text',
+        'editors/blockly/blocks/variablen',
+        'editors/blockly/blocks/web/opensensemap',
+        'editors/blockly/blocks/web/wifi',
+        'editors/blockly/blocks/zeit',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Arduino',
+	  key: 'edu-arduino',
+      link: {
+        type: 'generated-index',
+        title: 'Arduino',
+      },
+      items: [
+        'editors/arduino/installation-arduino-ide',
+        'editors/arduino/installation-bsp',
+        'boards/mcu/mcu-sensebox-io-library',
+        'editors/arduino/libraries-hinzufuegen',
+        'editors/arduino/first-steps',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Besonderheiten',
+	  key: 'edu-special-features',
+      link: {
+        type: 'generated-index',
+        title: 'Besonderheiten',
+      },
+      items: [
+        'misc/openRoberta',
+        'products/home/erweiterungen/home-erweiterung-LoRa',
+      ],
+    },
+  ],
 }
 
 export default homeSidebar
