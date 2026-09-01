e8ac75e5468bd7c12fd91437c603d244db62a2a2
Thiemann96 <e.thieme@reedu.de>
2026-09-01 12:30:35 +0200

    fix border on nav white

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -23,14 +23,14 @@ export function NavGreen({ topTabs, activeTab, onTabChange }) {
 
 export function NavWhite({ bottomTabs, activeTab, onTabChange }) {
   return (
-    <div className="bg-white border border-gray-200 rounded-xl shadow-sm p-1 mb-8 dark:bg-gray-900 dark:border-gray-800 dark:text-gray-200">
+    <div className="bg-white border border-gray-200 rounded-xl  p-1 mb-8 dark:bg-gray-900 dark:border-gray-800 dark:text-gray-200">
       <nav>
         <ul className="flex flex-row flex-wrap list-none gap-2 p-0 m-0">
           {bottomTabs.map((tab) => (
             <li key={tab} className="!m-0 flex-shrink-0">
               <button
                 onClick={() => onTabChange(tab)}
-                className={`px-4 py-3 rounded-lg  transition-all duration-200 ${
+                className={`px-4 py-3 rounded-lg  transition-all duration-200 border-none ${
                   activeTab === tab
                     ? 'text-[#45beed] border border-[#45beed] font-extrabold bg-green-50 dark:bg-emerald-700 dark:text-white rounded-xl '
                     : 'text-gray-700 font-medium hover:text-gray-900 hover:bg-gray-100 rounded-xl dark:text-gray-200 dark:hover:bg-gray-800'
